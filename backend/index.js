3627a3b4d60c0819ec3fe0912c56d273c310ae13
Dylan J. Gerrits <dgerrits@hipay.com>
2025-10-22 14:12:46 +0200

    Add error management and format

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const bookRoutes = require("./routes/book");
 
@@ -9,6 +10,15 @@ app.use(express.json());
 
 app.use("/api/books", bookRoutes);
 
-app.listen(4000, () => {
-    console.log("Server running on http://localhost:4000");
+app.use((error, request, response, next) => {
+  console.error("Une erreur est survenue.", error);
+  response.status(400).json({ error: error.message });
 });
+
+app.listen(4000, "127.0.0.1", () => {
+  console.log("Server running on http://127.0.0.1:4000");
+});
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
--- backend/models/books.js
+++ backend/models/books.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const booksSchema = mongoose.Schema({
-    title: { type: String, required: true},
-    author: { type: String, required: true},
-    year: { type: Number, required: true},
-    gender: { type: String, required: true},
-    note: { type: Number, required: true},
-    image: { type: String, required: true},
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  year: { type: Number, required: true },
+  gender: { type: String, required: true },
+  note: { type: Number, required: true },
+  image: { type: String, required: true },
 });
 
-module.exports = mongoose.model('Book', booksSchema);
+module.exports = mongoose.model("Book", booksSchema);

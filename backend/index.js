13ddbc418fa43aef677e0a5a67b4a0016353680c
Gabriel Gambotti <gabrielgambotti2006@gmail.com>
2025-10-22 11:39:01 +0200

    ajout de router

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const Book = require("./models/books");
+const bookRoutes = require("./routes/book");
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/api/books", (request, response, next) => {
-    delete request.body._id;
-    const book = new Book({
-        ...request.body
-    });
-    book.save()
-    .then(() => response.status(201).json({ message: 'Livre enregistré !' }))
-    .catch(error => response.status(400).json({ error }));
-});
-
-app.get("/api/books/:id", (request, response, next) => {
-    Book.findOne({ _id: request.params.id })
-    .then(book => response.status(200).json(book))
-    .catch(error => response.status(404).json({ error }));
-});
-
-app.get("/api/books", (request, response, next) => {
-    Book.find()
-    .then(books => response.status(200).json(books))
-    .catch(error => response.status(400).json({ error }));
-});
+app.use("/api/books", bookRoutes);
 
 app.listen(4000, () => {
     console.log("Server running on http://localhost:4000");
 });
-
-mongoose.connect('mongodb+srv://gabrielgambotti_db_user:Gabrielito11.@darkknight.rm2llp8.mongodb.net/?retryWrites=true&w=majority&appName=DarkKnight',)
-  .then(() => console.log('Connexion à MongoDB réussie'))
-  .catch(() => console.log('Connexion à MongoDB échouée'));
---
+++ backend/routes/book.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+
+const Book = require("../models/books");
+
+router.post("/", (request, response, next) => {
+    delete request.body._id;
+    const book = new Book({
+        ...request.body
+    });
+    book.save()
+    .then(() => response.status(201).json({ message: 'Livre enregistré !' }))
+    .catch(error => response.status(400).json({ error }));
+});
+
+router.put('/:id', (request, response, next) => {
+    Book.updateOne({ _id: request.params.id }, { ...request.body, _id: request.params.id })
+    .then(() => response.status(200).json({ message: 'Livre mis à jour !' }))
+    .catch(error => response.status(400).json({ error }));
+});
+
+router.delete('/:id', (request, response, next) => {
+    Book.deleteOne({ _id: request.params.id })
+    .then(() => response.status(200).json({ message: 'Livre supprimé !' }))
+    .catch(error => response.status(400).json({ error }));
+});
+
+router.get("/:id", (request, response, next) => {
+    Book.findOne({ _id: request.params.id })
+    .then(book => response.status(200).json(book))
+    .catch(error => response.status(404).json({ error }));
+});
+
+router.get("/", (request, response, next) => {
+    Book.find()
+    .then(books => response.status(200).json(books))
+    .catch(error => response.status(400).json({ error }));
+});
+
+module.exports = router;

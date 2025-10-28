231d21e9f6d12d889f4ce7646f9a5d2085496475
Gabriel Gambotti <gabrielgambotti2006@gmail.com>
2025-10-28 15:42:41 +0100

    vérification utilisateur pour login avec token intégrer + jsonwebtoken installer

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -17,5 +18,28 @@ exports.signUp = (request, response, next) => {
 }
 
 exports.login = (request, response, next) => {
-
+  User.findOne({ email: request.body.email })
+  .then(user => {
+    if (user === null) {
+      response.status(401).json({ message: "Utlisateur non trouvé" });
+    } else {
+      bcrypt.compare(request.body.password, user.password)
+      .then(valid => {
+        if (!valid) {
+          response.status(401).json({ message: "Mot de passe incorrect" });
+        } else {
+          response.status(200).json({
+            userId: user._id,
+            token: jwt.sign(
+              { userId: user._id },
+              "RANDOM_TOKEN_SECRET",
+              { expiresIn: "24h" }
+            )
+          });
+        }
+      })
+      .catch(error => {response.status(500).json({ error })});
+    }
+  })
+  .catch(error => response.status(500).json({ error }));
 }
